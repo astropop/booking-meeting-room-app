d34fb59c7e340db866cb02d7090c590ed2b81cb3
TAI\tailt <letuantai241@gmail.com>
2025-07-11 14:33:28 +0930

    feat:first commit main

---
+++ pages/meeting/index.tsx
@@ -0,0 +1,98 @@
+import styles from "./page.module.css";
+
+type studySpacesType = {
+  name: string;
+  url: string;
+};
+
+// bookable
+const studySpacesArr: studySpacesType[] = [
+  {
+    name: "410",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/f0fec07f-ff3d-4020-b248-5e8f7c1899df",
+  },
+  {
+    name: "411",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/a3ee741d-03fc-4f56-b1ab-d3c43b926c73",
+  },
+  {
+    name: "412",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/a345b77c-84c7-4aa9-a641-7349f229bd5a",
+  },
+  {
+    name: "413",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/66d827b9-501c-4008-91a1-a9cb96478f78",
+  },
+  {
+    name: "424",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/231eb791-091e-474c-8775-c09dfba56801",
+  },
+  {
+    name: "431",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/24b7c2fc-7ea8-4ace-a5e1-ab269a0dfae1",
+  },
+  {
+    name: "432",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/29fcac88-7f40-4cbf-9993-f67e4a5fcc00",
+  },
+  {
+    name: "512",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/c5522a4e-e2f8-4f52-8647-c79f5d8724a3",
+  },
+  {
+    name: "513",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/ee6ad42e-83a1-4271-b6fd-8fa844979f75",
+  },
+  {
+    name: "514",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/505528ee-71c9-4ffb-a714-227f5e99e6bd",
+  },
+  {
+    name: "515",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/aa28fad4-e4b1-4f98-9639-558fe0927a80",
+  },
+  {
+    name: "518",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/1b5289d8-dc55-45ea-bcc0-2018684310b5",
+  },
+  {
+    name: "519",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/1b5289d8-dc55-45ea-bcc0-2018684310b5",
+  },
+  {
+    name: "524",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/c3dd2fe7-041e-4df3-81ed-8bdede7c6903",
+  },
+  {
+    name: "525",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/8f5b3dc4-6852-4b13-85e4-9901bbbf0c22",
+  },
+  {
+    name: "526",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/bb7c7989-4095-4412-90a9-5fc5142b97b2",
+  },
+  {
+    name: "527",
+    url: "https://resourcebooker.flinders.edu.au/app/booking-types/e14fb159-2faf-411b-8125-70e08088b6f0/resources/9f107ff9-9e39-44c8-a971-d54891a7b546",
+  },
+];
+
+const BookableStudySpaces = () => {
+  const myList = studySpacesArr.map((item) => {
+    return (
+      <li>
+        <p>{item.name}</p>
+        <p>
+          <a href={item.url} target='_blank'>
+            Click here
+          </a>
+        </p>
+      </li>
+    );
+  });
+  return myList;
+};
+
+export default function Meeting() {
+  return <ul>{BookableStudySpaces()}</ul>;
+}
